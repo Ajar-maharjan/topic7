b40521806f3f5ed4404aeaca8707604a2e0837aa
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-12-20 12:20:24 +0545

    completed dictionary

--- app/src/main/java/com/novc/topic7/MainActivity.java
+++ app/src/main/java/com/novc/topic7/MainActivity.java
@@ -2,13 +2,18 @@ package com.novc.topic7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,17 +30,34 @@ public class MainActivity extends AppCompatActivity {
         lstDictionary = findViewById(R.id.lstDictionary);
         dictionary = new HashMap<>();
 
+        readFromFile();
+        ArrayAdapter adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet())
+        );
+        lstDictionary.setAdapter(adapter);
+
+        lstDictionary.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String key = parent.getItemAtPosition(position).toString();
+                String meaning = dictionary.get(key);
+                Intent intent = new Intent(MainActivity.this, MeaningActivity.class);
+                intent.putExtra("meaning", meaning);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void readFromFile(){
-        try{
-            FileInputStream fos = openFileInput("word.txt");
+    private void readFromFile() {
+        try {
+            FileInputStream fos = openFileInput("words.txt");
             InputStreamReader isr = new InputStreamReader(fos);
             BufferedReader br = new BufferedReader(isr);
             String line = "";
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 String[] parts = line.split("->");
-                dictionary.put(parts[0],parts[1]);
+                dictionary.put(parts[0], parts[1]);
             }
 
         } catch (IOException e) {
